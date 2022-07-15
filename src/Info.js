380358c3bbd60d36c38fbe710099e469073c5bf3
RATSENO <onestar1014@gmail.com>
2022-07-15 17:45:26 +0900

    useEffect

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickName, setNickname] = useState("");
+  useEffect(()=>{
+    console.log('렌더링이 완료되었습니다.');
+    console.log({
+        name,
+        nickName
+    })
+  });
 
   const onChangeName = (e) => {
     setName(e.target.value);

9c0dfc662e807eda7f2ecdb68a6b925b5b456639
ratseno <onestar1014@gmail.com>
2022-07-17 21:56:07 +0900

    useEffect - cleanUp 함수

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Info from "./Info";
 
 const App = () => {
   const [visible, setVisible] = useState(false);
-  return <div onClick={() => { setVisible(!visible) }}>
-    <button>{visible ? '숨기기' : '보이기'}</button>
+  return <div >
+    <button onClick={() => { setVisible(!visible) }}>{visible ? '숨기기' : '보이기'}</button>
     <hr />
     {visible && <Info />}
   </div>
